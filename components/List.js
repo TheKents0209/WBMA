af72db55e58631c1ff7fe95302bc858de3797cc1
Kenert <kenert2001@gmail.com>
2021-08-24 11:40:21 +0300

    taskB

--- components/List.js
+++ components/List.js
@@ -2,18 +2,23 @@ import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import {FlatList} from 'react-native';
 import ListItem from './ListItem';
+import {baseUrl} from '../utils/variables';
 
 const List = (props) => {
   const [mediaArray, setMediaArray] = useState([]);
-  const url =
-    'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+  const url = baseUrl;
 
   useEffect(() => {
     const loadMedia = async () => {
       try {
         const response = await fetch(url);
-        const json = await response.json();
-        setMediaArray(json);
+        const mediaIlmanThumbnailia = await response.json();
+        const kaikkiTiedot = mediaIlmanThumbnailia.map(async (media) => {
+          const response = await fetch(baseUrl + 'media/' + media.file_id);
+          const tiedosto = await response.json();
+          return tiedosto;
+        });
+        setMediaArray(await Promise.all(kaikkiTiedot));
       } catch (e) {
         console.log(e.message());
       }
